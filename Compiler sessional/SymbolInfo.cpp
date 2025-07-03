60d4479696b35b63a13bb1b4a5a96b742d26e3f8
RATUL <fmratul5@gmail.com>
2025-07-03 20:48:46 +0600

    final

--- Compiler sessional/SymbolInfo.cpp
+++ Compiler sessional/SymbolInfo.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define size 100
-#define id "202314402" // Your id, which is used to generate a hash value.
-#define lastdigitofid 2; // Then left shift each ASCII value by the last digit of your student ID,
+#define size 8
+#define id "202314402"
+#define lastdigit 2
 
 class SymbolInfo{
     public:
     string SymbolName;
     string symbolType;
-
 };
 
 int numberofchar(){
@@ -20,59 +19,61 @@ int numberofchar(){
     }
     sum=sum%9+1;
     return sum;
-}
+}   
 
-class symbolTable
-{
-public:
+class symbolTable{
     vector<SymbolInfo> hashtable[size];
-    int charnumber=numberofchar();
+    int charnumber = numberofchar();
 
-    // Function to calculate the hash index based on the symbol name
+    public:  
     int hashIndexfun(string name) {
         int sum=0;
-        for(int i = 0; i < charnumber; i++) {
+        for(int i = 0; i <charnumber; i++) {
             int index = name[i];
-            index= index<<lastdigitofid;
+            index= index<<2;
             sum += index;
         }
         int hashIndex = sum % size; 
         return hashIndex;
     }
-    // Function to insert a symbol into the hash table
+
+    int hashfunction(SymbolInfo sym){
+        string name = sym.SymbolName;
+        int hashIndex = hashIndexfun(name);
+        hashtable[hashIndex].push_back(sym);
+        return hashIndex;
+    }
+
     void insert(SymbolInfo sym){
-        pair<int,int> search=Lookup(sym.SymbolName); //lookup the symbol to check if it already exists
-        if(search.first != -1) { 
-            // If the symbol does not exist, insert it into the hash table
-            hashtable[search.first].push_back(sym);
-            cout <<"Inserted at position: " << search.first << ", " << search.second << endl;
-        } else {
-            cout << "Symbol already exists in the table." << endl;
+        pair<int, int> search = Lookup(sym.SymbolName);
+        if(search.first != -1) {
+            cout <<"Already exists"<< endl;
+            return;
         }
+        int hashIndex=hashfunction(sym);
+        cout<<"Inserted at position:"<<hashIndex<< ", " << search.second << endl;
     }
 
     pair<int,int> Lookup(string name){
         int hashIndex = hashIndexfun(name);
+        int i_row=-1;
         if(!hashtable[hashIndex].empty()) {
-            int i_row=-1;
             SymbolInfo s;
             for(auto &sym : hashtable[hashIndex]) {
                 i_row++;
                 if(sym.SymbolName == name) {
                     s= sym;
-                    break;
+                    return make_pair(hashIndex, i_row);
                 }
             }
-            if(i_row != -1) {
-                return make_pair(hashIndex, i_row);
-            } else {
-                return make_pair(-1, -1); // Symbol not found
+                return make_pair(-1,i_row+1);
             }
-        } else {
-            return make_pair(-1, -1); // Symbol not found
+        else {
+            return make_pair(-1, 0);
         }
-    }
 
+    }
+    
     void Delete(string name){
         pair<int,int> search=Lookup(name);  // Lookup the symbol to check if it exists
         if(search.first != -1) {
@@ -80,13 +81,14 @@ public:
             hashtable[search.first].erase(hashtable[search.first].begin() + search.second);
             cout << "Deleted from " << search.first << ", " << search.second << endl;
         } else {
-            cout << "Symbol not found in the table." << endl;
+            cout << "Not Found" << endl;
         }
     }
+
     void printf(){
         for(int i = 0; i < size; i++) {
-            cout<<i << " ->";
             if(!hashtable[i].empty()) {
+                cout<<i << " ->";
                 for(auto &sym : hashtable[i]) {
                     cout<<"<" << sym.SymbolName << ", " << sym.symbolType << "> ";
                 }
@@ -98,17 +100,21 @@ public:
 
 int main() {
     symbolTable st;
+    // Example usage of the symbol table
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    string operation, name, type;
-    while (cin>>operation>> name >> type) {
-        SymbolInfo sym;
-        sym.SymbolName = name;
-        sym.symbolType = type;
-        if(operation == "I") {
+    string operation;
+    string name, type;
+    while (cin >> operation) {
+        if (operation == "I") {
+            cin >> name >> type;
+            SymbolInfo sym;
+            sym.SymbolName = name;
+            sym.symbolType = type;
             st.insert(sym);
         }
-         else if (operation == "L") {
+        else if (operation == "L") {
+            cin >> name;
             pair<int,int> search = st.Lookup(name);
             if(search.first != -1) {
                 cout << "Found at " << search.first << ", " << search.second << endl;
@@ -116,16 +122,18 @@ int main() {
                 cout << "Not found." << endl;
             }
         }
-         else if (operation == "D") {
+        else if (operation == "D") {
+            cin >> name;
             st.Delete(name);
         }
-         else  if (operation == "P") {
+        else if (operation == "P") {
             st.printf();
         }
         else {
             cout << "Invalid operation." << endl;
         }
     }
-    
+
+
     return 0;
 }
